afecea79512807e332deeb80ecca56434fe58131
Marta Bianchini <marta.bianchini24@gmail.com>
2023-01-25 19:19:49 +0000

    Updated fetchPokemon

---
+++ my-project/pokédex/api.js
@@ -0,0 +1,21 @@
+const fetchPokemon = (pokemonName) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+    .then((response) => response.json())
+    .then((data) => {
+    let pokemon = {
+      name: data.name,
+      id: data.id,
+      height: data.height,
+      weight: data.weight,
+      types: data.types.map((element) => {
+        return element.type.name;
+      })
+    };
+    return pokemon
+  });
+};
+
+fetchPokemon('charizard')
+.then((pokemon) => console.log(pokemon))
+
+module.exports = fetchPokemon;
