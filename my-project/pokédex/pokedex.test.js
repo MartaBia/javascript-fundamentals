a6a079eaedb6c7feaf053043e0bac38f237eb84e
Marta Bianchini <marta.bianchini24@gmail.com>
2023-01-27 14:44:44 +0000

    minor modification to pokedex.test

--- my-project/pokédex/pokedex.test.js
+++ my-project/pokédex/pokedex.test.js
@@ -16,4 +16,17 @@ describe('Pokedex', () => {
         done();
       });
   });
+
+  // Sort it out later
+  // it('Returns the pokemon in the pokedex once is catched', (done) => {
+  //   const pokedex = new Pokedex();
+  //   pokedex.catch('pikachu')
+  //     .then(() => pokedex.catch('jigglypuff'))
+  //     .then(() => pokedex.catch('eevee'))
+  //     .then((pokemon) => {
+  //       expect(pokemon.all()[0].name).toBe('pikachu');
+  //       expect(pokemon.all()[1].name).toBe('jigglypuff');
+  //       done();
+  //     });
+  // });
 });
